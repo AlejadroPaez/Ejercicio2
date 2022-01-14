879c2ad226149ec78837de10b125b1b637931e55
Alejandro Paez <46059841+AlejadroPaez@users.noreply.github.com>
2022-01-13 20:20:41 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 2</title>
+</head>
+<body>
+    <div>
+        <h2>Introduce los siguientes datos</h2>
+        <form action="procesar.php" method="POST">
+            <label for="dato1">Primero número: </label><br>
+            <input type="number" name="dato1"><br>
+            <label for="dato2">Segundo número: </label><br>
+            <input type="number" name="dato2"><br>
+            <input type="submit" id="procesar">
+        </form>
+    </div>
+    
+    
+</body>
+</html>
